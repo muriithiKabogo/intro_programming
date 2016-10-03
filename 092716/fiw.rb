521e6ca8d66554b155ca32b0ec173f2c6fe01899
muriithiKabogo <duncan.muriithi@meltwater.org>
2016-10-03 08:23:38 +0000

    fourth commit

--- 092716/fiw.rb
+++ 092716/fiw.rb
@@ -1,6 +1,9 @@
- 
- def converts(y)
-	tentonineteen=["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+
+
+def converts(y)
+	tentonineteen=["""ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+	tohundred=["","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred"]
+	wordstoten=["""zero","one","two","three","four","five","six","seven","eight","nine"]
 	tos=y.to_s
 	l=tos.length
 	spec=tos[0]
@@ -15,13 +18,90 @@
 			
 			if xspec==1&&xspec2==b
 					puts"#{i}"
-				
+			elsif xspec==2&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"twenty #{j} "
+					end
+
+				end
+			break
+
+			elsif xspec==3&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"thirty #{j} "
+					end
+
+				end
+			break
+
+		elsif xspec==4&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"forty #{j} "
+					end
+
+				end
+			break
+		elsif xspec==5&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"fifty #{j} "
+					end
+
+				end
+			break
+		elsif xspec==6&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"sixty #{j} "
+					end
+
+				end
+			break
+		elsif xspec==7&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"seventy #{j} "
+					end
+
+				end
+			break
+		elsif xspec==8&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"Eighty #{j} "
+					end
+
+				end
+			break
+		elsif xspec==9&&xspec2==b
+				wordstoten.each do |j|
+					c=wordstoten.index(j)
+					if b==c
+						puts"ninety #{j} "
+					end
+
+				end
+			break
+
+
 			end		
 		end
 
-		
 
 	end
+
+
+		
 end
 
 def convert(y)
@@ -45,7 +125,4 @@ def convert(y)
     
 
 end
-
-convert(19)
-
-
+convert(98)
